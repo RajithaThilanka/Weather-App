796f4d543af3aff45bf891363d01b308fed3c91f
Rajitha <rajithat1998@gmail.com>
2023-04-12 21:27:34 +0530

    update

--- client/src/Components/Home/HomePageComp.jsx
+++ client/src/Components/Home/HomePageComp.jsx
@@ -1,32 +1,42 @@
-import React from 'react'
-import {Box,Stack, Typography,TextField,Button,Autocomplete  }from '@mui/material';
+import React from 'react';
+import { Box, Stack, Typography, TextField, Button } from '@mui/material';
 import { useState } from 'react';
+import { getWeather } from '../../api/userRequests';
 
-
-export default function () {
-
-  const locationSuggestions = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Philadelphia'];
-  const [value, setValue] = useState('');
-  const handleInputChange = (event, newValue) => {
-    setValue(newValue);
+export default function WeatherForm() {
+  const [location, setLocation] = useState('');
+  
+  const handleLocationChange = (event) => {
+    setLocation(event.target.value);
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getWeather(location)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   return (
     <div>
-      <Box style={{ backgroundColor: 'white' }} sx={{ border: 1, width: "500px", height: "600px",boxShadow: 3,borderRadius:2 }}>
-        
-        <Stack direction="column" spacing={3}  marginTop={3} sx={{alignItems:"center" ,justifyContent:"center"}}>
-          <Typography variant="h5">
-            Realtime Weather Detection System
-          </Typography>
+      <Box
+        style={{ backgroundColor: 'white' }}
+        sx={{ border: 1, width: '500px', height: '600px', boxShadow: 3, borderRadius: 2 }}
+      >
+        <Stack direction="column" spacing={3} marginTop={3} sx={{ alignItems: 'center', justifyContent: 'center' }}>
+          <Typography variant="h5">Realtime Weather Detection System</Typography>
           <Stack direction="row" spacing={2}>
-            <TextField id="outlined-basic" label="Enter Location" variant="outlined" />
-            <Button variant="contained">Submit</Button>
+            <TextField label="Enter Location" variant="outlined" value={location} onChange={handleLocationChange} />
+            <Button variant="contained" onClick={handleSubmit}>
+              Submit
+            </Button>
           </Stack>
-      
         </Stack>
-    </Box>
+      </Box>
     </div>
-  )
+  );
 }
